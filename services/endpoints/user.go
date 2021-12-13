d0a10f738f365c946e2b68e2df79d2ce2a660374
SeraphJACK <seraphjack@outlook.com>
2021-12-13 12:05:47 +0800

    考完接着摸.png

--- services/endpoints/user.go
+++ services/endpoints/user.go
@@ -1,18 +1,19 @@
 package endpoints
 
 import (
-	"encoding/base64"
 	"github.com/ProjectAnni/anniv-go/model"
 	"github.com/gin-gonic/gin"
+	uuid "github.com/satori/go.uuid"
 	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 	"net/http"
 	"regexp"
 	"strings"
+	"time"
 )
 
 var usernameReg = regexp.MustCompile("^[A-Za-z0-9-_]{5,11}$")
-var emailReg = regexp.MustCompile("^[A-Za-z0-9.]@((?!-))(xn--)?[a-z0-9][a-z0-9-_]{0,61}[a-z0-9]{0,1}\\.(xn--)?([a-z0-9\\-]{1,61}|[a-z0-9-]{1,30}\\.[a-z]{2,})$")
+var emailReg = regexp.MustCompile("^[A-Za-z0-9.]@((?!-))(xn--)?[a-z0-9][a-z0-9-_]{0,61}[a-z0-9]?\\.(xn--)?([a-z0-9\\-]{1,61}|[a-z0-9-]{1,30}\\.[a-z]{2,})$")
 
 func User(ng *gin.Engine, db *gorm.DB) {
 	g := ng.Group("/api/user")
@@ -47,7 +48,7 @@ func User(ng *gin.Engine, db *gorm.DB) {
 		}
 		user := model.User{
 			Username: form.Username,
-			Password: base64.StdEncoding.EncodeToString(hash),
+			Password: string(hash),
 			Email:    form.Email,
 			Nickname: form.Nickname,
 			Avatar:   form.Avatar,
@@ -61,7 +62,35 @@ func User(ng *gin.Engine, db *gorm.DB) {
 	})
 
 	g.POST("login", func(ctx *gin.Context) {
-		// TODO
+		form := LoginForm{}
+		err := ctx.Bind(&form)
+		if err != nil {
+			ctx.JSON(http.StatusOK, illegalParams("malformed login form"))
+			return
+		}
+		user := model.User{}
+		if db.Where("email = ?", form.Email).First(&user).RowsAffected == 0 {
+			ctx.JSON(http.StatusOK, userNotFound())
+			return
+		}
+		err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(form.Password))
+		if err != nil {
+			ctx.JSON(http.StatusOK, wrongPassword())
+			return
+		}
+		session := model.Session{
+			UserID:       user.ID,
+			User:         user,
+			SessionID:    uuid.NewV4().String(),
+			UserAgent:    ctx.Request.UserAgent(),
+			LastAccessed: time.Now(),
+		}
+		if err = db.Save(&session).Error; err != nil {
+			ctx.JSON(http.StatusOK, writeErr(err))
+			return
+		}
+		ctx.SetCookie("session", session.SessionID, 86400, "/", "", true, true)
+		ctx.JSON(http.StatusOK, resOk(userInfo(user)))
 	})
 }
 
@@ -77,6 +106,22 @@ func illegalEmail(msg string) Response {
 	return Response{
 		Status:  EmailUnavailable,
 		Message: msg,
+		Data:    nil,
+	}
+}
+
+func userNotFound() Response {
+	return Response{
+		Status:  UserNotExist,
+		Message: "user not exist",
+		Data:    nil,
+	}
+}
+
+func wrongPassword() Response {
+	return Response{
+		Status:  InvalidPassword,
+		Message: "email and password does not match",
 		Data:    nil,
 	}
 }
