0efebba1676a25393ed4623fea91d7e036a6b39f
SeraphJACK <seraphjack@outlook.com>
2021-12-13 12:54:16 +0800

    Fix regex

--- services/user.go
+++ services/user.go
@@ -8,12 +8,11 @@ import (
 	"gorm.io/gorm"
 	"net/http"
 	"regexp"
-	"strings"
 	"time"
 )
 
 var usernameReg = regexp.MustCompile("^[A-Za-z0-9-_]{5,11}$")
-var emailReg = regexp.MustCompile("^[A-Za-z0-9.]@(xn--)?[a-z0-9][a-z0-9-_]{0,61}[a-z0-9]?\\.(xn--)?([a-z0-9\\-]{1,61}|[a-z0-9-]{1,30}\\.[a-z]{2,})$")
+var emailReg = regexp.MustCompile("^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])$")
 
 func EndpointUser(ng *gin.Engine, db *gorm.DB) {
 	g := ng.Group("/api/user")
@@ -24,7 +23,7 @@ func EndpointUser(ng *gin.Engine, db *gorm.DB) {
 			ctx.JSON(http.StatusOK, illegalParams("malformed register form"))
 			return
 		}
-		if !usernameReg.MatchString(form.Username) || strings.Contains(form.Username, "\n") {
+		if !usernameReg.MatchString(form.Username) {
 			ctx.JSON(http.StatusOK, illegalUsername("Username must match [A-Za-z0-9-_]{5,11}"))
 			return
 		}
@@ -32,7 +31,7 @@ func EndpointUser(ng *gin.Engine, db *gorm.DB) {
 			ctx.JSON(http.StatusOK, illegalUsername("username already taken"))
 			return
 		}
-		if !emailReg.MatchString(form.Email) || strings.Contains(form.Email, "\n") {
+		if !emailReg.MatchString(form.Email) {
 			ctx.JSON(http.StatusOK, illegalEmail("invalid email"))
 			return
 		}
