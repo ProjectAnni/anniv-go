b03f62e893a4f0bb0e3c32a5647099ce40337126
SeraphJACK <seraphjack@outlook.com>
2021-12-24 23:26:05 +0800

    这日志好烦人啊.jpg

--- services/favorite.go
+++ services/favorite.go
@@ -45,8 +45,9 @@ func EndpointFavorite(ng *gin.Engine) {
 			ctx.JSON(http.StatusOK, illegalParams("malformed music form"))
 			return
 		}
+		cnt := int64(0)
 		if db.Where("user_id = ? AND album_id = ? AND disc_id = ? AND track_id = ?",
-			user.ID, form.AlbumID, form.DiscID, form.TrackID).First(&model.FavoriteMusic{}).RowsAffected != 0 {
+			user.ID, form.AlbumID, form.DiscID, form.TrackID).Count(&cnt); cnt != 0 {
 			ctx.JSON(http.StatusOK, Response{
 				Status:  AlreadyExist,
 				Message: "music already exist",
