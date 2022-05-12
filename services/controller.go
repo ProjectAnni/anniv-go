1244252ce01676fc886256106c22a9b263f7098a
SeraphJACK <seraphjack@outlook.com>
2022-05-12 20:04:01 +0800

    token migrator

--- services/controller.go
+++ services/controller.go
@@ -2,17 +2,21 @@ package services
 
 import (
 	"errors"
+	"flag"
 	"github.com/ProjectAnni/anniv-go/config"
 	"github.com/ProjectAnni/anniv-go/model"
 	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
+	uuid "github.com/satori/go.uuid"
 	"gorm.io/driver/postgres"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"log"
 	"os"
 )
 
 var db *gorm.DB
+var migrateTokens = flag.Bool("migrate-tokens", false, "")
 
 func Start(listen string) error {
 	var err error
@@ -25,6 +29,47 @@ func Start(listen string) error {
 		if !errors.Is(sqlite.ErrConstraintsNotImplemented, err) {
 			return errors.New("failed to migrate db: " + err.Error())
 		}
+	}
+
+	if *migrateTokens {
+		log.Println("Start migrating tokens...")
+		var users []model.User
+		db.Find(&users)
+		for _, user := range users {
+			err := db.Transaction(func(tx *gorm.DB) error {
+				err := tx.Unscoped().Where("controlled IS NULL").Delete(&model.Token{}).Error
+				if err != nil {
+					return err
+				}
+				err = tx.Unscoped().Where("controlled").Delete(&model.Token{}).Error
+				if err != nil {
+					return err
+				}
+				tokens, err := signUserTokens(user.Email)
+				if err != nil {
+					return err
+				}
+				for _, token := range tokens {
+					err := tx.Create(&model.Token{
+						TokenID:    uuid.NewV4().String(),
+						Name:       token.Name,
+						URL:        token.URL,
+						Token:      token.Token,
+						Priority:   token.Priority,
+						UserID:     user.ID,
+						Controlled: true,
+					}).Error
+					if err != nil {
+						return err
+					}
+				}
+				return nil
+			})
+			if err != nil {
+				log.Fatalf(err.Error())
+			}
+		}
+		os.Exit(0)
 	}
 
 	initMiddleware()
